52666ac42591808832302f4d7013507a5585388a
HGroome97 <harrison.groome@outlook.com>
2018-10-30 14:57:24 +0000

    json conversion

--- src/main/java/com/qa/app/app.java
+++ src/main/java/com/qa/app/app.java
@@ -1,22 +1,32 @@
 package com.qa.app;
 
+import java.io.File;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.qa.account.Account;
 import com.qa.account.AccountManager;
+import com.qa.jsonobject.JsonObject;
 
 public class App {
 	private App(){
 		
 	}
 	public static void main(String[] args) {
-		System.out.println("hello world to the standard out");
+		//System.out.println("hello world to the standard out");
 		Account a1  = new Account("Harrison", "Groome", 1234);
 		Account a2  = new Account("Alvin", "Alvin", 1235);
 		AccountManager am  = new AccountManager();
 		am.addAccount(a1);
 		am.addAccount(a2);
-		System.out.println(am.getAccounts());
-		am.deleteAccount(1234);
-		System.out.println(am.getAccounts());
+		//System.out.println(am.getAccounts());
+		//am.deleteAccount(1234);
+		//System.out.println(am.getAccounts());
+		
+		JsonObject jo = new JsonObject();
+		jo.ObjectJsonFile(a1);
+		System.out.println(jo.JsonToObjectFile("c:\\file.json"));
+		
+		System.out.println(am.mapToJson(am.getAccounts()));
 	}
 
 }
