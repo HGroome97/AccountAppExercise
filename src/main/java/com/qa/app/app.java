c931a65a7eece7f2f6029e7873e49f2704a9b419
HGroome97 <harrison.groome@outlook.com>
2018-10-30 10:37:14 +0000

    s

---
+++ src/main/java/com/qa/app/app.java
@@ -0,0 +1,10 @@
+package com.qa.app;
+
+public class app {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world to the standard out");
+	}
+
+}
