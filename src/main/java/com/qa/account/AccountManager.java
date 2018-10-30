81d2542dfcef6cc93f3943b1ea86ab717d862040
HGroome97 <harrison.groome@outlook.com>
2018-10-30 11:35:10 +0000

    branch account

--- src/main/java/com/qa/account/AccountManager.java
+++ src/main/java/com/qa/account/AccountManager.java
@@ -1,17 +1,22 @@
 package com.qa.account;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class AccountManager {
-	private HashMap<Integer, Account> accountMap = new HashMap<Integer, Account>();
+	private HashMap<Integer, Account> accountMap = new HashMap<>();
 
 	public void addAccount(Account a) {
 		accountMap.put(a.getAccountNumber(), a);
 	}
 	
 	
-	public HashMap<Integer, Account> getAccounts() {
+	public Map<Integer, Account> getAccounts() {
 		return accountMap;
 	}
-
+	
+	public void deleteAccount(int accountNum) {
+		accountMap.remove(accountNum);
+	}
+	
 }
